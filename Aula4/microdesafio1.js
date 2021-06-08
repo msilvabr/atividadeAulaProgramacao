9968bb3a3e964b6a942ecb98a697962bb21ec76f
msilva <matheuscorreios@gmail.com>
2021-06-08 11:45:41 -0300

    Atividades Microdesafio

---
+++ Aula4/microdesafio1.js
@@ -0,0 +1,8 @@
+let status = "true"
+
+if(status=="true"){
+    console.log("O valor é true - verdadeiro")
+}
+else{
+    console.log("O valor é false - falso")
+}
---
+++ Aula4/microdesafio2.js
@@ -0,0 +1,8 @@
+let linguagem = "javascript"
+
+if(linguagem == "javascript"){
+    console.log("Estou aprendendo!")
+}
+else{
+    console.log("Aprenderei mais tarde!")
+}

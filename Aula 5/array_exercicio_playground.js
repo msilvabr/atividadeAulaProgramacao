9cb6bff078949687f10232c8787a04348a8c8fd9
msilva <matheuscorreios@gmail.com>
2021-06-11 14:13:29 -0300

    Exercicio Array

---
+++ Aula 5/array_exercicio_playground.js
@@ -0,0 +1,58 @@
+// let pessoa = ["João", "Marcão", 22, 1.80, false]
+// console.log(pessoa)
+
+// let pessoa = ["matheus", 22, 1.94]
+// console.log(pessoa)
+
+// //push
+//     pessoa.push(22)
+//     console.log(pessoa)
+
+// //pop
+//     pessoa.pop()
+//     console.log(pessoa)
+
+// //unshift
+//     pessoa.unshift(22)
+//     console.log(pessoa)
+
+// //shift
+//     pessoa.shift()
+//     console.log(pessoa)
+
+// //indexOf
+//     console.log(pessoa.indexOf(1.94))
+
+// //lastindexOf
+//     console.log(pessoa.lastIndexOf)
+
+// //join
+//     console.log(pessoa.join(" , "))
+
+//ATIVIDADE PLAYGROUND
+
+let listaDeCompras = ["arroz", "leite", "ovos", "manteiga", "pão"]
+
+// //join
+//     console.log("O método JOIN, acrescenta espaços que o usúario pode definir")
+//     console.log(listaDeCompras.join(", "))
+
+// //Pop
+//     console.log("O comando Pop elimina e retorna o último item de um array")
+//     console.log(listaDeCompras.pop())
+//     console.log(listaDeCompras)
+
+//Push
+    // console.log("O comando 'push' adiciona elementos ao final do array, e retorna o novo comprimento do mesmo")
+    // console.log(listaDeCompras.push("chocolate"))
+    // console.log(listaDeCompras)
+
+//shift
+    // console.log("Elimina o primeiro elemento do Array, e o retorna ao mesmo tempo")
+    // console.log(listaDeCompras.shift())
+    // console.log(listaDeCompras)
+
+//unshift
+    console.log("Acrescenta um ou mais elementos no início de um Array, e ao mesmo tempo retorna o novo comprimento")
+    console.log(listaDeCompras.unshift("cebola"))
+    console.log(listaDeCompras)

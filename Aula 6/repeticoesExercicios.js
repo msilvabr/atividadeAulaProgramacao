d13a9822a3b34260239ebc3fb86f2ef6352a8598
Matheus Silva <85120589+msilvabr@users.noreply.github.com>
2021-06-12 20:03:12 -0300

    exercicios loop

---
+++ Aula 6/repeticoesExercicios.js
@@ -0,0 +1,22 @@
+//microdesafio 1
+
+// for(let world =1; world <=5; world++){
+//     console.log("Hello World!")
+// }
+
+//microdesafio  2
+
+// for(let num=1; num <=10; num++){
+//     console.log(num++)
+// }
+
+//microdesafio 3
+
+// for(let num=0; num <=10; num++){
+//     console.log("8 X "+num+ " = " +num*8)
+// }
+ 
+
+
+
+

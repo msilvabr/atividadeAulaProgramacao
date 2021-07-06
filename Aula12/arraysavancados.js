b825e8b88715514ba4c6bf54675c1b6896b18f05
Matheus Silva <85120589+msilvabr@users.noreply.github.com>
2021-07-06 12:09:51 -0300

    exercicios aula 06.07

---
+++ Aula12/arraysavancados.js
@@ -0,0 +1,42 @@
+// array .map()
+// array.map(fuction(elemnto){})
+
+// let num = [2,4,6]
+
+// let dobro = num.map(function(numero){
+//     return numero*2
+// })
+// console.log(dobro)
+
+//array filter()
+// array.filer(fuction(elemento))
+
+// let idades=[22,8,17,14,30,18]
+
+// let maiores=idades.filter(function(idade){
+//     return idade>=18
+// })
+// console.log(maiores)
+
+//array reduce VER MELHOR
+//array.reduce(fuction(acumulador,elemento))
+
+// let numeros=[1,2]
+// let soma=numeros.reduce(function(acumulador,numero){
+//     return acumulador+numero
+// })
+// console.log(soma)
+
+//array .forEach()
+//array.forEach(function(elemento))
+// var paises=['Brasil','Cuba','Peru','China']
+// paises.forEach(function(pais){
+//     console.log(pais)
+// })
+
+//.slice()
+//array.slice(inicio, fim)
+// let numeros=[1,2,3,4,5,6]
+// let subArray=numeros.slice(0,4)
+// console.log(subArray)
+
---
+++ Aula12/exercicios.js
@@ -0,0 +1,27 @@
+//Microdesafios
+//1 .map()
+// let numeros=[2,4,6,8,10]
+// let impar=numeros.map(function(impares){
+//     return impares+1
+// })
+// console.log(impar)
+
+//2 .filter()
+// let nomes=['Carlinha','Maria','Joaquina','Leticia','Mototaxista','Maria']
+// let marias=nomes.filter(function(nome){
+//     return nome=='Maria'
+// })
+// console.log(marias)
+
+//3 .reduce()
+// let numeros=[1,2,3,4,5]
+// let numerosFormatados=numeros.reduce(function(acumulador,numero){
+//     return acumulador+'-'+numero
+// })
+// console.log(numerosFormatados)
+
+//4 .forEach()
+// let animais=['Camaleão','Baleia','Gato','Cachorro']
+// animais.forEach(function(bichos){
+//     console.log('O nome do animal é '+bichos)
+// })
